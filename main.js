521b50c7ffcf10389c6557a1a40e1c8b8267eb54
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-13 15:02:36 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    // Nome repo: rest-todoolean
+    // Creare una piccola applicazione web per gestire una lista di "todo". Le operazioni principali che devo essere implementate sono:
+    // la lettura di tutti i todo
+    // l'inserimento di un nuovo todo
+    // la cancellazione di un todo.
+    // BONUS: gestire la modifica di un todo.
+    // Questa è la lista con tutte le porte disponibili, ce n'è una per ciascuno, così ognuno può lavorare tranquillamente nel proprio spazio.
+    // Vi ricordo, per motivi tecnici, di non cancellare tutti i todo!
+    // Buon lavoro!
+
+// var template_html = $("#day-template").html();
+// var template_function = Handlebars.compile(template_html);
+
+})
